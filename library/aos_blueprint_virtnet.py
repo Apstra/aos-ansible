e0c9b21c9a4497d13f8eec0cea0f5b0b563750c9
Jeremy Schulman <jeremy@apstra.com>
2016-11-22 15:24:16 -0500

    added blueprint virtual-network support

---
+++ library/aos_blueprint_virtnet.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# Copyright 2014-present, Apstra, Inc. All rights reserved.
+#
+# This source code is licensed under End User License Agreement found in the
+# LICENSE file at http://www.apstra.com/community/eula
+
+
+import os
+import json
+
+from ansible.module_utils.basic import AnsibleModule
+
+try:
+    from apstra.aosom.session import Session
+    from apstra.aosom.exc import LoginError, SessionRqstError
+    from apstra.aosom.amods.virtnets import VirtualNetworks
+    HAS_AOS_PYEZ = True
+except ImportError:
+    HAS_AOS_PYEZ = False
+
+
+def ensure_present(module, blueprint):
+    src_file = module.params['src']
+
+    # TODO: use the built-in ansible module mech for this
+    # ...
+    if not src_file:
+        module.fail_json(msg="'src' parameter required")
+
+    try:
+        datum = json.load(open(src_file))
+    except Exception as exc:
+        module.fail_json(msg="unable to load src file '%s': %s" %
+                             (src_file, exc.message))
+
+    name = datum['display_name']
+    vnets = VirtualNetworks(blueprint)
+    vnets.digest()
+    this_vnet = vnets[name]
+
+    if this_vnet.exists:
+        module.exit_json(changed=False, contents=this_vnet.value)
+
+    try:
+        this_vnet.create(datum)
+    except SessionRqstError as exc:
+        module.fail_json(msg="umable to create virtual-network '%s': '%s'" %
+                             (name, exc.message))
+
+    vnets.digest()
+    module.exit_json(changed=True, contents=vnets[name].value)
+
+
+def ensure_absent(module, blueprint):
+    pass
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            session=dict(required=True, type="dict"),
+            blueprint_name=dict(required=True),
+            src=dict(required=False),
+            state=dict(choices=['present', 'absent'], default='present'))
+    )
+
+    if not HAS_AOS_PYEZ:
+        module.fail_json(msg='aos-pyez is not installed.  Please see details '
+                             'here: https://github.com/Apstra/aos-pyez')
+
+    margs = module.params
+    auth = margs['session']
+    aos, blueprint, param = [None] * 3
+
+    try:
+        aos = Session()
+        aos.api.resume(url=auth['url'], headers=auth['headers'])
+    except LoginError as exc:
+        module.fail_json(msg='unable to login to AOS API: %s' % str(exc))
+
+    blueprint = aos.Blueprints[margs['blueprint_name']]
+
+    if not blueprint.exists:
+        module.fail_json(msg='blueprint %s does not exist.\n'
+                             'known blueprints are [%s]'%
+                             (margs['blueprint_name'],
+                              ','.join(aos.Blueprints.names)))
+
+
+
+    {
+        'present': ensure_present,
+        'absent': ensure_absent
+
+    }[margs['state']](module, blueprint)
+
+
+if __name__ == '__main__':
+    main()
